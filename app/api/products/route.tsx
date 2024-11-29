172611ea84af9d3b888f8a6901de8d2d7bbdd389
dezuF <dezuF@users.noreply.github.com>
2024-11-28 23:39:29 -0400

    feat: implement product form with custom UI and API operations

---
+++ app/api/products/route.tsx
@@ -0,0 +1,43 @@
+import Product from "@/lib/models/Product";
+import { connectToDB } from "@/lib/mongoDB";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+    try {
+        const { userId } = await auth ()
+
+        if (!userId){
+            return new NextResponse ("Unauthorizes", { status: 401 });
+        }
+        
+        await connectToDB()//
+
+        const { title, description, media, category, collections, tags, sizes, colors, price, expense} = await req.json();
+
+        if(!title || !description || !media || !category || !price || !expense){
+            return new NextResponse("Not enough data to create a product", {status: 400 });
+        }
+
+        const newProduct = await Product.create({
+            title,
+            description,
+            media,
+            category,
+            collections,
+            tags,
+            sizes,
+            colors,
+            price,
+            expense,
+        });
+
+        await newProduct.save();
+
+        return NextResponse.json(newProduct, {status: 200});
+
+    } catch (err) {
+        console.log("[products_POST", err);
+        return new NextResponse ("Internal Error", { status: 500 });
+    }
+}
