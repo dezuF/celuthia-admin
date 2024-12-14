5bd3e8eaeb2ce13a8e1896fef132c9f3ffb83d82
dezuF <dezuF@users.noreply.github.com>
2024-12-14 14:48:42 -0400

    fix

--- components/custom ui/Loader.tsx
+++ components/custom ui/Loader.tsx
@@ -4,20 +4,20 @@ const Loader = () => {
       <div 
         className="animate-spin rounded-full border-t-4 border-solid h-12 w-12"
         style={{
-          borderTopColor: 'rgb(59, 130, 246)', // Azul
+          borderTopColor: 'rgb(59, 130, 246)',
           animation: 'spin 1s linear infinite, colorChange 2s ease-in-out infinite',
         }}
       ></div>
       <style jsx>{`
         @keyframes colorChange {
           0% {
-            border-top-color: #3b82f6; /* Azul */
+            border-top-color: #3b82f6;
           }
           50% {
-            border-top-color: #f472b6; /* Rosado */
+            border-top-color: #f472b6;
           }
           100% {
-            border-top-color: #3b82f6; /* Azul */
+            border-top-color: #3b82f6; 
           }
         }
 
